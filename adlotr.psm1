f904a49116a294b09904fe6f580eef99993d3e94
ehmiiz <emil_temp92@live.se>
2022-05-13 16:03:38 +0200

    y

--- adlotr.psm1
+++ adlotr.psm1
@@ -1,3 +1,23 @@
-$Data = Import-Csv .\lotr_characters.csv
+<#
+    TODO Complete :D
+    Work in progress
+#>
 
-$Races = $Data.race | Where-Object {$_.Length -gt 2} | Select-Object -Unique
+$DataChar = Import-Csv .\data\lotr_characters.csv
+# $DataScripts = Import-Csv .\data\lotr_scripts.csv
+
+$Races = $DataChar.race | Where-Object {$_.Length -gt 2} | Select-Object -Unique
+
+$ADData = Get-ADDomain
+
+$DataChar | ForEach-Object {
+    $_.name
+
+    $Splat = @{
+        Name = $_.Name
+        UserPrincipleName = "$($_.Name.replace(" ","."))@$($ADData.Forest)"
+        
+    }
+    $Splat
+    # New-ADUser @Splat
+}
